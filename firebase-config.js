0f08d92b0c9933f92c91a3c5d158fac21dbb3fa5
y5496694 <113575280+y5496694@users.noreply.github.com>
2025-06-13 13:22:36 +0900

    Remove firebase and list

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyAkbIpMW8gy3YUquXwaunPj3ZgnuOUUx_E",
-  authDomain: "googer-class-paper.firebaseapp.com",
-  projectId: "googer-class-paper",
-  storageBucket: "googer-class-paper.firebasestorage.app",
-  messagingSenderId: "897629708262",
-  appId: "1:897629708262:web:b02a0aeb6034aa5aa68027",
-  measurementId: "G-XDVXQ32YZV"
-};
